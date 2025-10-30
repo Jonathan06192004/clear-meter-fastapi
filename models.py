6eed971d585a646b4b6e7052c3d797557d5756c4
Jonats <jayop216@gmail.com>
2025-10-30 10:00:37 +0800

    updated models.py

--- models.py
+++ models.py
@@ -2,10 +2,13 @@ from sqlalchemy import Column, Integer, DateTime, func
 from database import Base
 
 class WaterReading(Base):
-    __tablename__ = "water_readings_bridge"
+    __tablename__ = "water_readings"
 
-    id = Column(Integer, primary_key=True, index=True)
+    reading_id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, index=True)
     device_id = Column(Integer, index=True)
     reading_5digit = Column(Integer, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    previous_reading = Column(Integer, default=0)
+    current_reading = Column(Integer, default=0)
+    consumption = Column(Integer, default=0)
+    timestamp = Column(DateTime, server_default=func.now())
